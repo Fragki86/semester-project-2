0afd439f8d6094ef888d1bb7ad37da680e50158c
Georgios Fragkias <geo.fragkias@gmail.com>
2021-12-03 20:23:21 +0100

    greeting and add product

--- js/modules/layout/nav.js
+++ js/modules/layout/nav.js
@@ -7,13 +7,17 @@ export function nav() {
   
   const username = getUser();
 
-  console.log(username)
-  let authorisationLink = `<a href="login.html" id="loggedUser">Login</a>`
+  let authorisationLink = `
+                            <a href="login.html"><i class="far fa-user"></i></a>
+                            <a href="login.html" id="loggedUser">Login</a>`
   let adminLinks = "";
-
+  
   if (username) {
-    authorisationLink = `<span>Hi, ${username}</span>`
-    adminLinks = `<li class="nav-item dropdown">
+    authorisationLink = `<a href="#"><i class="fas fa-user"></i></a>
+                        <span class="greeting-span">Hi, ${username}<i class="fas fa-sign-out-alt"></i></span>`
+    adminLinks = `
+    <div class="nav-separator"></div>
+    <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
       Admin Panel<i class="fas fa-chevron-circle-down"></i>
     </a>
@@ -23,6 +27,7 @@ export function nav() {
     </ul>
   </li>`
   }
+
 
   navMenu.innerHTML = `
                 <nav>
@@ -44,61 +49,37 @@ export function nav() {
                         <a href="products.html" class="${pathname === "/products.html" ? "active" : ""}">Products</a>
                       </li>
                       ${adminLinks}
-                      
                     </ul>
                     <div class="login-container">
-                      <a href="login.html"><i class="fas fa-user"></i></a>
                       ${authorisationLink}
                     </div>
                 </nav>
                 `
 
+if (username){
+const logOutBtn = document.querySelector(".greeting-span")
+logOutBtn.addEventListener ("click", logOut);
+
+  function logOut() {
+    const signOutConfirmation = confirm("Are you sure you want to sign out?");
+
+    if (signOutConfirmation) {
+      localStorage.removeItem("user");
+
+      location.href = "/index.html";
+    }
+  }
+}
+
 
 
 const burgerBtn = document.querySelector("#burgerBtn");
 const navMobile = document.querySelector("nav ul");
-const navList = document.querySelectorAll("nav li");
 
 burgerBtn.addEventListener ("click", fadeInMenu);
-// burgerBtn.addEventListener ("click", stylishLinks);
 
 function fadeInMenu() {
     navMobile.classList.toggle("nav-visible");
+
 }
-
-// function stylishLinks() {
-//     navList.forEach(function(links, length){
-//         if (links.style.animation) {
-//             links.style.animation = '';
-//         } else {
-//             links.style.animation = `fade-in 0.3s forwards  ${length / 15 + 0.3}s`
-//         }
-
-//     })
-// }
 }
-
-{/* <nav class="navbar navbar-expand-lg navbar-light">
-                  <div class="container-fluid">
-                    <a href="shopping-cart.html" id="cart">   
-                      <div id="counter"></div>
-                      <i class="fas fa-shopping-bag"></i>
-                    </a>
-                    <a class="navbar-brand" href="home" id="logo">
-                      <img src="images/logo.svg" alt="Eco-kids Logo">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                      <ul class="navbar-nav text-center">
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="products.html">Products</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </nav> */}
---
+++ js/pages/addProduct/addProduct.js
@@ -0,0 +1,16 @@
+import { nav } from "../../modules/layout/nav.js"
+import { footer } from "../../modules/layout/footer.js"
+import { counter } from "../../modules/utilities/counter.js"
+import { api } from "../../modules/utilities/api.js"
+
+nav();
+footer();
+counter();
+
+function addProduct() {
+  
+
+
+}
+addProduct()
+
