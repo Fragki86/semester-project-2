d29a9de1ae00b95255f58a6df9edd58bb7baa201
Georgios Fragkias <geo.fragkias@gmail.com>
2021-12-06 09:36:51 +0100

    radio

--- js/pages/addProduct/addProduct.js
+++ js/pages/addProduct/addProduct.js
@@ -2,6 +2,8 @@ import { nav } from "../../modules/layout/nav.js"
 import { footer } from "../../modules/layout/footer.js"
 import { counter } from "../../modules/utilities/counter.js"
 import { api } from "../../modules/utilities/api.js"
+import { systemMessage } from "../../modules/utilities/systemMessage.js"
+import { getToken } from "../../localStorage/tokenUser.js"
 
 nav();
 footer();
@@ -11,20 +13,61 @@ const addProductForm = document.querySelector("#addProductForm");
 const title = document.querySelector("#titleInput");
 const price = document.querySelector("#priceInput");
 const description = document.querySelector("#descriptionInput");
-const radioRuttons = document.querySelector("#radio-buttons");
+// const featured = document.querySelector('input[name="featured"]:checked');
 const image = document.querySelector("#imageInput");
+const messageContainer = document.querySelector(".message-container");
 
 addProductForm.addEventListener("submit", validateForm)
 
 function validateForm(event) {
   event.preventDefault();
-
+  let featuredValue = document.querySelector('input[name="featured"]:checked').value;
   const titleValue = title.value.trim();
   const priceValue = price.value.trim();
   const descriptionValue = description.value.trim();
+  // let featuredValue = featured.value;
+  // alert(featuredValue);
+  // if (featured === "no") {
+  //   console.log("correct")
+  // } else {
+  //   console.log("error")
+  // }
+  // console.log(event)
 
+  if (titleValue.length < 3 || priceValue < 1 || descriptionValue.length < 8) {
+    return systemMessage("error", "All fields required", ".message-container")
+  }
+
+
+  addProduct(titleValue, priceValue, descriptionValue, featuredValue)
+}
+
+
+async function addProduct(title, price, description, featured) {
+  const newUrl = api + "/products";
+  const token = getToken();
+  const data = JSON.stringify({title: title, price: price, description: description, featured: featured});
+  const options = {
+    method: "POST",
+    body: data,
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+  };
+
+  try {
+    const response = await fetch(newUrl, options);
+    const results = await response.json();
+
+    console.log(results);
+
+  } catch(error) {
+    systemMessage("warning", "Something went wrong", ".message-container");
+    console.log(error);
+  }
 
 
 }
-validateForm()
 
+// validateForm();
